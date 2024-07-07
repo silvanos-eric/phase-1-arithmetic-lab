3b1dd218c5c8368e02983c2ffbb75dc7705491d4
Silvanos Eric <ericoderito@gmail.com>
2024-07-07 13:57:49 +0300

    feat: add new arithmetic operations
    - Add multiplication of two numbers (num1 and num2)
    - Generate a random number between 1 and 10
    - Add modulus operation to divide num3 by num4
    - Found the maximum value from a set of numbers (5, 10, 15, 20)

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// mod
+const num3 = 8;
+const num4 = 2;
+const mod = num3 / num4;
+
+// max
+const max = Math.max(5, 10, 15, 20);
